b56b74065e47a1aff618fc4b2fd3b749ec1396a6
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2015-12-07 16:30:25 -0500

    Add large hypergraph transversal correctness tests

--- src/test/java/org/compsysmed/ocsana/internal/algorithms/mhs/MMCSAlgorithmTest.java
+++ src/test/java/org/compsysmed/ocsana/internal/algorithms/mhs/MMCSAlgorithmTest.java
@@ -13,20 +13,27 @@ import static org.junit.Assert.*;
 
 // Java imports
 import java.util.*;
+import java.io.*;
 
 // OCSANA imports
 import org.compsysmed.ocsana.internal.algorithms.mhs.Hypergraph;
 import org.compsysmed.ocsana.internal.algorithms.mhs.MMCSAlgorithm;
 
 public class MMCSAlgorithmTest {
-    MMCSAlgorithm alg;
+    MMCSAlgorithm algFull;
+    MMCSAlgorithm algCutoff;
     Hypergraph smallHypergraph;
 
     @Before
     public void setUp () {
         // Set up the test environment here
         // In particular, initialize any shared variables
-        alg = new MMCSAlgorithm();
+        algFull = new MMCSAlgorithm();
+        algFull.useMaxCardinality = false;
+
+        algCutoff = new MMCSAlgorithm();
+        algCutoff.useMaxCardinality = true;
+        algCutoff.maxCardinalityBInt.setValue(6);
 
         List<List<Integer>> smallHypergraphEdges = new ArrayList<>();
         smallHypergraphEdges.add(Arrays.asList(1, 2, 5));
@@ -40,17 +47,30 @@ public class MMCSAlgorithmTest {
         // Tear down the test environment here
         // In particular, null out any shared variables so the garbage
         // collector can trash them
-        alg = null;
+        algFull = null;
+        algCutoff = null;
+
         smallHypergraph = null;
     }
 
     @Test
     public void smallHypergraphTransversalShouldWork () {
-        Hypergraph smallHypergraphTransversal = alg.transversalHypergraph(smallHypergraph);
+        Hypergraph smallHypergraphTransversal = algFull.transversalHypergraph(smallHypergraph);
         assertEquals("Small hypergraph transversal should have 5 edges", 5, smallHypergraphTransversal.numEdges());
 
         for (BitSet transversal: smallHypergraphTransversal) {
             assertTrue("Small hypergraph should be transversed by each element of its transversal", smallHypergraph.isTransversedBy(transversal));
         }
+    }
+
+    @Test
+    public void hugeHypergraphCutoffTransversalShouldWork ()
+        throws FileNotFoundException, IOException, NumberFormatException {
+        File HER2File = new File(getClass().getResource("/mhs-data/HER2.all.dat").getFile());
+        Hypergraph H = new Hypergraph(HER2File);
+
+        Hypergraph T = algCutoff.transversalHypergraph(H);
+
+        assertEquals("Huge hypergraph transversal with cutoff 6 should have 320 edges", 320, T.numEdges());
     }
 }
